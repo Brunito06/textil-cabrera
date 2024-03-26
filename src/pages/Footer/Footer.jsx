d6faeff00a8f1c42cddd519e83630cd2e34f1ee8
Bruno Jorba <brujorba@gmail.com>
2024-03-26 13:51:12 -0300

    primer commit de bruno

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -3,8 +3,38 @@ import './Footer.css'
 
 const Footer = () =>{
     return(
-        <footer>
-            footer
+        <footer >
+            <section>
+                <ul className='ul-footer'>
+                    <li className='footer-bold'>Inicio</li>
+                    <li>Quienes somos</li>
+                    <li>Producto</li>
+                    <li>Contacto</li>
+                </ul>
+                <ul className='ul-footer'>
+                    <li className='footer-bold'>Productos</li>
+                    <li>Estopas</li>
+                    <li>Trapos</li>
+                    <li>TNT</li>
+                    <li>Guatas</li>
+                </ul>
+                <ul className='ul-footer'>
+                    <li className='footer-bold'>Nosotros</li>
+                    <li>Link</li>
+                    <li>Link</li>
+                </ul>
+                <ul className='ul-footer'>
+                    <li className='footer-bold'>Contacto</li>
+                    <li><img src="" alt="" /><p>+598 2288 5630</p></li>
+                    <li><img src="" alt="" /><p>+598 2288 8083</p></li>
+                    <li><img src="" alt="" /><p>tcc@adinet.com.uy</p></li>
+                </ul>
+                <div>
+                    <img id='wsp-logo' src="" alt="" />
+                    <img id='ig-logo' src="" alt="" />
+            </div>
+            </section>
+                    <p id='copyright'>© 2024 Textil Cabrera SRL | Desarrollo web a cargo de Emiliano Rodriguez & Bruno Jorba</p>
         </footer>
     );
 }
--- src/pages/NavBar/NavBar.jsx
+++ src/pages/NavBar/NavBar.jsx
@@ -1,11 +1,24 @@
 //import styles
 import './NavBar.css'
+import logo from './assets/logo.png'
+import esp from './assets/esp.png'
+import eng from './assets/eng.png'
 
 const NavBar = () =>{
     return(
-        <div className='home'>
-            NavBar
-        </div>
+        <nav id='navBar'>
+            <img id='logo' src={logo} alt="" />
+            <ul id='navItems'>
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Nosotros</li>
+                <li>Contacto</li>
+            </ul>
+            <button id='language'>
+                <img id='flags' src={esp} alt="" />
+                <p>Español</p>
+            </button>
+        </nav>
     );
 }
 

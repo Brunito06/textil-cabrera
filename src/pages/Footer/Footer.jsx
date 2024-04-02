a2207bff86d12b5896755eff8174e9403e9f5521
Bruno Jorba <brujorba@gmail.com>
2024-04-02 20:02:35 -0300

    footer updates

--- src/pages/Footer/Footer.jsx
+++ src/pages/Footer/Footer.jsx
@@ -16,24 +16,24 @@ const Footer = () =>{
             <section>
                 <ul className='ul-footer'>
                     <li className='footer-bold'><Link className='footerLinks' onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}} to="/">Inicio</Link></li>
-                    <li>Quienes somos</li>
-                    <li><Link className='footerLinks' to="/productos">Producto</Link></li>
-                    <li><Link className='footerLinks' to="/contacto">Contacto</Link></li>
+                    <li><Link className='footerLinks' onClick={() => {window.scroll({top: 560, left: 0, behavior: "smooth",});}} to="/">Quienes somos</Link></li>
+                    <li><Link className='footerLinks' onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}} to="/productos">Producto</Link></li>
+                    <li><Link className='footerLinks' onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}} to="/contacto">Contacto</Link></li>
                 </ul>
                 <ul className='ul-footer'>
-                    <li className='footer-bold'><Link className='footerLinks' to="/productos">Productos</Link></li>
-                    <li>Estopas</li>
-                    <li>Trapos</li>
-                    <li>TNT</li>
-                    <li>Guatas</li>
+                    <li className='footer-bold'><Link className='footerLinks' onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}} to="/productos">Productos</Link></li>
+                    <li><Link className='footerLinks' onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}} to="/productos">Estopas</Link></li>
+                    <li><Link className='footerLinks' onClick={() => {window.scroll({top: 1420, left: 0, behavior: "smooth",});}} to="/productos">Trapos</Link></li>
+                    <li><Link className='footerLinks' onClick={() => {window.scroll({top: 2800, left: 0, behavior: "smooth",});}} to="/productos">TNT</Link></li>
+                    <li><Link className='footerLinks' onClick={() => {window.scroll({top: 3600, left: 0, behavior: "smooth",});}} to="/productos">Guatas</Link></li>
                 </ul>
                 <ul className='ul-footer'>
                     <li className='footer-bold'>Nosotros</li>
-                    <li>Quienes somos</li>
-                    <li>Envios</li>
+                    <li><Link className='footerLinks' onClick={() => {window.scroll({top: 560, left: 0, behavior: "smooth",});}} to="/">Quienes somos</Link></li>
+                    <li><Link className='footerLinks' onClick={() => {window.scroll({top: 560, left: 0, behavior: "smooth",});}} to="/">Envios</Link></li>
                 </ul>
                 <ul className='ul-footer'>
-                    <li className='footer-bold'><Link className='footerLinks' to="/contacto">Contacto</Link></li>
+                    <li className='footer-bold'><Link className='footerLinks' onClick={() => {window.scroll({top: 0, left: 0, behavior: "smooth",});}} to="/contacto">Contacto</Link></li>
                     <li className='contact-footer'><img className='contact-logos' src={tel} alt="" /><p>+598 2288 5630</p></li>
                     <li className='contact-footer'><img className='contact-logos' src={tel} alt="" /><p>+598 2288 8083</p></li>
                     <li className='contact-footer'><img className='contact-logos' src={mail} alt="" /><p>tcc@adinet.com.uy</p></li>
